ac7386249a15a4f3f0bdfa95bf0a904a6b0a55f5
Uladzimir86 <olodja@rambler.ru>
2021-02-22 11:59:50 +0300

    feat: add solution to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array)) return 0;
+  let min = 0;
+  array.forEach(item => min = (min > item ? item: min));
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array)) return 0;
+  let max = 0;
+  array.forEach(item => max = (max < item ? item: max));
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)) return 0;
+  let sum = 0;
+  array.forEach(item => sum +=item );
+  if (array.length == 0) return 0;
+    else return sum/array.length;
 }
